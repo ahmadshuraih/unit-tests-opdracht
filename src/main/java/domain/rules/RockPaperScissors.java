c4f6127c2f27479f08619eee4a71df763f93964a
Ahmad Shuraih <43374600+ahmadshuraih@users.noreply.github.com>
2020-04-05 16:39:07 +0200

    De opdracht is uitgewerkt en werkt zonder fouten

--- src/main/java/domain/rules/RockPaperScissors.java
+++ src/main/java/domain/rules/RockPaperScissors.java
@@ -5,34 +5,16 @@ import domain.Result;
 import domain.Rules;
 
 public class RockPaperScissors implements Rules {
-    @Override
-    public Result decide(Move mine, Move theirs) {
-        switch (mine) {
-            case ROCK:
-                switch (theirs) {
-                    case SCISSORS:
-                        return Result.WIN;
-                    case PAPER:
-                        return Result.LOSE;
-                }
-
-            case PAPER:
-                switch (theirs) {
-                    case ROCK:
-                        return Result.WIN;
-                    case SCISSORS:
-                        return Result.LOSE;
-                }
-
-            case SCISSORS:
-                switch (theirs) {
-                    case PAPER:
-                        return Result.WIN;
-                    case ROCK:
-                        return Result.LOSE;
-                }
-        }
-
-        return Result.DRAW;
-    }
+	@Override
+	public Result decide(Move mine, Move theirs) {
+		if ((mine == Move.ROCK && theirs == Move.SCISSORS) || (mine == Move.PAPER && theirs == Move.ROCK)
+				|| (mine == Move.SCISSORS && theirs == Move.PAPER)) {
+			return Result.WIN;
+		} else if ((mine == Move.ROCK && theirs == Move.PAPER) || (mine == Move.PAPER && theirs == Move.SCISSORS)
+				|| (mine == Move.SCISSORS && theirs == Move.ROCK)) {
+			return Result.LOSE;
+		} else {
+			return Result.DRAW;
+		}
+	}
 }
